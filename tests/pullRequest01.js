c2e7bb69b47f132c92d0d968628af59e34ce6c3f
Grecia Machaca <Grecia3093@msn.com>
2022-04-25 19:16:14 -0400

    Completing second task

--- tests/pullRequest01.js
+++ tests/pullRequest01.js
@@ -1,6 +1,7 @@
 module.exports = {
     'Demo test unosquare' : function(browser) {
       const gettingStartedSelector = '#navigation a[href="https://nightwatchjs.org/guide/getting-started/"]';
+      const gettingStartedTitle = 'Developer Guide | Nightwatch.js';
       const searchSelector = '.DocSearch-Button-Placeholder';
       const searchInput = '#docsearch-input';
       const searchText = 'Asserts';
@@ -8,19 +9,26 @@ module.exports = {
       const apiSelector = '#navbartop a[href="/api/"]';
       const blogSelector = '#navbartop a[href="https://nightwatchjs.org/blog/"]';
       const footerSelector = '.address';
+      const footerText = 'Nightwatch.js was created in Oslo, Norway by Pineview.io – an independent software consultancy; it is now being maintained by all these fine people with help from all our contributors.';
 
       browser
         .windowMaximize()
         .url('https://nightwatchjs.org/')
+        .assert.urlContains('nightwatchjs.org')
         .waitForElementVisible('body')
         .click(gettingStartedSelector)
+        .assert.titleEquals(gettingStartedTitle, 'Title is correct')
         .click(searchSelector)
         .setValue(searchInput, searchText)
+        .assert.visible(searchInput, 'Text is visible')
         .click(chooseResult+'0')
-        //.assert.textContains(apiSelector, 'API Reference') 
+        .assert.textContains(apiSelector, 'API') 
         .click(blogSelector)
-        .moveToElement(footerSelector, 150, 150);
-
+        .getLocation(footerSelector, function(){
+          this.assert.visible(footerSelector);
+          this.moveToElement(footerSelector, 0,0);
+        });
+      browser.assert.textContains(footerSelector, footerText, 'Footer contains text');
       browser.end();
     }
   }
